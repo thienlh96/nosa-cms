a2630cd0917bdbcca26b5148add1fea41d6833d6
thienlh96 <44220134+thienlh96@users.noreply.github.com>
2018-10-31 17:35:39 +0700

    đé

--- public/cms-js/conf.js
+++
@@ -1,7 +0,0 @@
-google.charts.load('current', {
-        'packages':["corechart","geochart","table","bar"],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-        'mapsApiKey': 'AIzaSyB5Ku0e4U5pBW1GXdduH01dZ6W7w7yCLD4&callback'
-      });
-//var serviceUrl ="https://nongsanvn.herokuapp.com/";
