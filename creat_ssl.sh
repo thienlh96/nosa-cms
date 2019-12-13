c6a486cc57a95d3bbe2dbfc9b2143a119ed8dab0
thienlh96 <44220134+thienlh96@users.noreply.github.com>
2019-12-13 14:24:06 +0700

    a

--- creat_ssl.sh
+++ creat_ssl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 chmod +x /home/ec2-user/nosa-cms/cron_ssl.sh;
-certonly --webroot --webroot-path=/var/www/html --email thientt@kyc.net.vn --agree-tos --no-eff-email --staging -d ttt.vaytieudungtheoluong247.com  -d www.ttt.vaytieudungtheoluong247.com;
 docker-compose stop webserver;
 mkdir dhparam;
 sudo openssl dhparam -out /home/ec2-user/nosa-cms/dhparam/dhparam-2048.pem 2048;
