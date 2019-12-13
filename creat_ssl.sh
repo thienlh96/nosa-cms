e9bf8a92b76cf34213b79b300d149c96862361f6
thienlh96 <44220134+thienlh96@users.noreply.github.com>
2019-12-13 10:04:26 +0700

    ssh

--- creat_ssl.sh
+++ creat_ssl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+chmod +x /home/ec2-user/nosa-cms/cron_ssl.sh
 certonly --webroot --webroot-path=/var/www/html --email thientt@kyc.net.vn --agree-tos --no-eff-email --staging -d ttt.vaytieudungtheoluong247.com  -d www.ttt.vaytieudungtheoluong247.com;
 docker-compose stop webserver;
 mkdir dhparam;
 sudo openssl dhparam -out /home/ec2-user/nosa-cms/dhparam/dhparam-2048.pem 2048;
-rm nginx-conf/nginx.conf;
-mv /home/ec2-user/ssl.conf /home/ec2-user/nginx_conf/;
+rm /home/ec2-user/nosa-cms/nginx-conf/nginx.conf;
+mv /home/ec2-user/nosa-cms/ssl.conf /home/ec2-user/nosa-cms/nginx_conf/;
 docker-compose up -d --force-recreate --no-deps webserver;
--- cron_ssl.sh
+++ cron_ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/ecw-user/nosa-cms/docker-compose.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f /home/ecw-user/nosa-cms/docker-compose.yml kill -s SIGHUP webserver
+/usr/local/bin/docker-compose -f /home/ec2-user/nosa-cms/docker-compose.yml run certbot renew --dry-run \
+&& /usr/local/bin/docker-compose -f /home/ec2-user/nosa-cms/docker-compose.yml kill -s SIGHUP webserver
